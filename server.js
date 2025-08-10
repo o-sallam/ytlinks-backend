18808b4da89ac03479e91f26ac7688afa9f121cd
o-sallam <omar.sallam.dev@gmail.com>
2025-08-10 16:13:36 +0200

    up2

--- server.js
+++ server.js
@@ -71,4 +71,8 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/health', (req, res) => {
+  res.status(200).send({ status: 'OK' });
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
