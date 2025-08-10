a8f71cfeeeebfd3fd276c4409138b38ee871dc3d
o-sallam <omar.sallam.dev@gmail.com>
2025-08-10 16:57:07 +0200

    railwayconfigs

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/api/youtube_search', async (req, res) => {
     // Launch puppeteer browser
     const browser = await puppeteer.launch({
       headless: 'new',
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
     });
     
     const page = await browser.newPage();
